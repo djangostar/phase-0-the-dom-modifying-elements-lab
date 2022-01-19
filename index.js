f3ad49db42f8dbe3bb18324f63033806728fa1db
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-01-19 15:24:17 -0800

    commit tree 239fa52d79ed4cb09f2b32a2b1a894b500331802

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById('main').remove('main')
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Django is the champion'
